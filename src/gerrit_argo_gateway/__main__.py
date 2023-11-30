49d92b883b4424d66120f0723a3e08cf4b2bd665
Fabian Wiesel <fabian.wiesel@sap.com>
2023-11-30 10:46:37 +0100

    Add and fix more strict linting
    It is easier to do that early then later.

--- src/gerrit_argo_gateway/__main__.py
+++ src/gerrit_argo_gateway/__main__.py
@@ -1,22 +1,27 @@
+"""
+Main module to be invoked via python -m gerrit_argo_gateway.
+
+Sets up logging and the gateway and hooks up the signal handlers to stop the thread.
+"""
 import asyncio
 import logging
 import signal
 
 from . import GerritGateway
 
-async def main():
+
+async def _main() -> None:
     gateway = GerritGateway(subscriptions=["patchset-created", "comment-added"])
     loop = asyncio.get_running_loop()
 
-    def stop(*args, **kwargs):
+    def stop(*args, **kwargs) -> None:  # noqa: ARG001
         gateway.stop()
 
     for sig in [signal.SIGINT, signal.SIGTERM]:
-        # signal.signal(sig, stop)
         loop.add_signal_handler(sig, stop)
 
     await gateway()
 
 
 logging.basicConfig(level=logging.INFO)
-asyncio.run(main())
+asyncio.run(_main())
